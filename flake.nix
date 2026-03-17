b4cd2c80fc16d8313110d207bb637c93cab02f73
LodWKobku <lodwkobku@gmail.com>
2026-03-17 20:21:27 +0100

    fixed formating

--- flake.nix
+++ flake.nix
@@ -14,12 +14,12 @@
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./configuration.nix
-	home-manager.nixosModules.home-manager
-        {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.user = ./home.nix;
-	}
+        home-manager.nixosModules.home-manager
+            {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.user = ./home.nix;
+        }
       ];
     };
   };
