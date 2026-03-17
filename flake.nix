9c78787bffeb40473e011d46d37b4a670c7e6564
LodWKobku <lodwkobku@gmail.com>
2026-03-17 20:21:01 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+	home-manager.nixosModules.home-manager
+        {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.user = ./home.nix;
+	}
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,8 @@
+{ config, pkgs, ... }:
+
+{
+	home.username = "user";
+ 	home.homeDirectory = "/home/user";
+	home.stateVersion = "25.11";
+}
+
