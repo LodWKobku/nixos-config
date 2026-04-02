06fb2645e954c880a76e6b58cf87f01ed6485c0d
LodWKobku <lodwkobku@gmail.com>
2026-04-02 16:36:53 +0200

    added printing support

--- configuration.nix
+++ configuration.nix
@@ -112,8 +112,24 @@
      };
    };
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  # Printing
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      cups-filters
+      cups-browsed
+      cnijfilter2 # Canon Pixma drivers
+    ];
+  };
+  hardware.printers.ensurePrinters = [
+    {
+      name = "Canon_TS3300_series";
+      location = "";
+      deviceUri = "usb://Canon/TS3300%20series?serial=4C3AAE&interface=1";
+      model = "canone3300.ppd";
+    }
+  ];
+  
 
   # Enable sound with pipewire.
   hardware.bluetooth.enable = true;
