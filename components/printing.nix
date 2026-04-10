3b725ddee25602e6e9fae3ef43a2635463461ecd
LodWKobku <lodwkobku@gmail.com>
2026-04-10 19:05:08 +0200

    moved printing and keyboard configuration into seperete files

---
+++ components/printing.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+  
+  # Printing
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [
+      cups-filters
+      cups-browsed
+      cnijfilter2 # Canon Pixma drivers
+    ];
+  };
+  hardware.printers.ensurePrinters = [
+    {
+      name = "Canon_TS3300_series";
+      location = "";
+      deviceUri = "usb://Canon/TS3300%20series?serial=4C3AAE&interface=1";
+      model = "canone3300.ppd";
+    }
+  ];
+}
--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,8 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./components/keyboard.nix
+      ./components/printing.nix
     ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
@@ -27,21 +29,6 @@
   # Set your time zone.
   time.timeZone = "Europe/Warsaw";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pl_PL.UTF-8";
-    LC_IDENTIFICATION = "pl_PL.UTF-8";
-    LC_MEASUREMENT = "pl_PL.UTF-8";
-    LC_MONETARY = "pl_PL.UTF-8";
-    LC_NAME = "pl_PL.UTF-8";
-    LC_NUMERIC = "pl_PL.UTF-8";
-    LC_PAPER = "pl_PL.UTF-8";
-    LC_TELEPHONE = "pl_PL.UTF-8";
-    LC_TIME = "pl_PL.UTF-8";
-  };
-
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
@@ -49,87 +36,6 @@
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  i18n.inputMethod = {
-     type = "fcitx5";
-     enable = true;
-     fcitx5 = {
-      waylandFrontend = true;
-      ignoreUserConfig = true;
-      addons = with pkgs; [
-        fcitx5-gtk             # alternatively, kdePackages.fcitx5-qt
-        qt6Packages.fcitx5-chinese-addons  # table input method support
-        fcitx5-m17n # Pinyin tones support
-        fcitx5-nord            # a color theme
-      ];
-      settings.inputMethod = {
-        # Group 1
-        GroupOrder."0" = "Default";
-        "Groups/0" = {
-          Name = "Default";
-          "Default Layout" = "us";
-          DefaultIM = "keyboard-pl";
-        };
-        "Groups/0/Items/0".Name="keyboard-pl";
-
-        # Group 2 (for chinese)
-        GroupOrder."1" = "Chinski";
-        "Groups/1" = {
-          Name = "Chinski";
-          "Default Layout" = "us";
-          DefaultIM = "m17n_zh_pinyin";
-        };
-        "Groups/1/Items/0".Name="pinyin";
-        "Groups/1/Items/1".Name="m17n_zh_pinyin";
-      };
-      settings.globalOptions = {
-        "Hotkey" = {
-          EnumerateWithTriggerKeys="True";
-          EnumerateSkipFirst="False";
-          ModifierOnlyKeyTimeout="250";
-        };
-        "Hotkey/TriggerKeys" = {
-          "0"="Alt+Shift+Shift_R";
-          "1"="Zenkaku_Hankaku";
-          "2"="Hangul";
-        };
-        "Hotkey/ActivateKeys" = {
-          "0"="Hangul_Hanja";
-        };
-        "Hotkey/DeactivateKeys" = {
-          "0"="Hangul_Romaja";
-        };
-        "Hotkey/AltTriggerKeys" = {
-          "0"="Shift_L";
-        };
-      };
-     };
-   };
-
-  # Printing
-  services.printing = {
-    enable = true;
-    drivers = with pkgs; [
-      cups-filters
-      cups-browsed
-      cnijfilter2 # Canon Pixma drivers
-    ];
-  };
-  hardware.printers.ensurePrinters = [
-    {
-      name = "Canon_TS3300_series";
-      location = "";
-      deviceUri = "usb://Canon/TS3300%20series?serial=4C3AAE&interface=1";
-      model = "canone3300.ppd";
-    }
-  ];
-  
 
   # Enable sound with pipewire.
   hardware.bluetooth.enable = true;
