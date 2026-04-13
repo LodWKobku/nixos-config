c49d82575fce61e66e01d0d94ce279572a029fc7
LodWKobku <lodwkobku@gmail.com>
2026-04-13 12:49:07 +0200

    moved installing programs to seperate file

---
+++ components/programs.nix
@@ -0,0 +1,47 @@
+{ config, pkgs, ... }:
+{
+  # Installed packages
+  environment.systemPackages = with pkgs; [
+     pkgs.gh # Github CLI
+     pkgs.gemini-cli
+     pkgs.aider-chat-full # aider gemini cli alternative
+     pkgs.atlauncher # Minecraft Launcher
+     pkgs.python313Packages.kernels # Huggingface CLI
+     pkgs.python313Packages.modelscope # Modelscope CLI
+     pkgs.pear-desktop # Youtube music app
+     pkgs.gnome-calculator # Calculator
+     pkgs.gnome-clocks # Clock
+     pkgs.heroic # Heroic game launcher
+  ];
+
+  # Firefox.
+  programs.firefox.enable = true;
+
+  # Nh (nix helper)
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 5";
+    flake = "/home/user/Documents/nixos-nowe"; # sets NH_OS_FLAKE variable for you
+  };
+
+  # Steam
+  programs.steam = {
+    enable = true; # Master switch, already covered in installation
+    remotePlay.openFirewall = true;  # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports for Source Dedicated Server hosting
+    # Other general flags if available can be set here.
+  };
+
+  # OBS
+  programs.obs-studio = {
+    enable = true;
+    enableVirtualCamera = true;
+  };
+
+  #Flatpak
+  services.flatpak.enable = true;
+  services.flatpak.packages = [
+      "org.freedownloadmanager.Manager"
+  ];
+}
--- configuration.nix
+++ configuration.nix
@@ -10,6 +10,7 @@
       ./hardware-configuration.nix
       ./components/keyboard.nix
       ./components/printing.nix
+      ./components/programs.nix
     ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
@@ -60,25 +61,8 @@
     ];
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
-
-  # Installed packages
-  environment.systemPackages = with pkgs; [
-     pkgs.gh # Github CLI
-     pkgs.gemini-cli
-     pkgs.aider-chat-full # aider gemini cli alternative
-     pkgs.atlauncher # Minecraft Launcher
-     pkgs.python313Packages.kernels # Huggingface CLI
-     pkgs.python313Packages.modelscope # Modelscope CLI
-     pkgs.pear-desktop # Youtube music app
-     pkgs.gnome-calculator # Calculator
-     pkgs.gnome-clocks # Clock
-     pkgs.heroic # Heroic game launcher
-  ];
 
   # Stylix
   stylix = {
@@ -90,34 +74,6 @@
         hash = "sha256-/HAtpGwLxjNfJvX5/4YZfM8jPNStaM3gisK8+ImRmQ4=";
       };
   };
-
-  # Nh (nix helper)
-  programs.nh = {
-    enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 5";
-    flake = "/home/user/Documents/nixos-nowe"; # sets NH_OS_FLAKE variable for you
-  };
-
-  # Steam
-  programs.steam = {
-    enable = true; # Master switch, already covered in installation
-    remotePlay.openFirewall = true;  # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports for Source Dedicated Server hosting
-    # Other general flags if available can be set here.
-  };
-
-  # OBS
-  programs.obs-studio = {
-    enable = true;
-    enableVirtualCamera = true;
-  };
-
-  #Flatpak
-  services.flatpak.enable = true;
-  services.flatpak.packages = [
-      "org.freedownloadmanager.Manager"
-  ];
 
   system.stateVersion = "25.11";
 }
