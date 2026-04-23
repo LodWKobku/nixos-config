d8c6e02f9f4a4efa41f462e6af1e7605719ac3b1
LodWKobku <lodwkobku@gmail.com>
2026-04-23 15:27:06 +0200

    created startup script

--- components/home-manager/hyprland.nix
+++ components/home-manager/hyprland.nix
@@ -1,6 +1,14 @@
 { inputs, config, pkgs, ... }:
+let
+    startupScript = pkgs.pkgs.writeShellScriptBin "start" ''
+      ${pkgs.waybar}/bin/waybar
+    '';
+in
 {
     wayland.windowManager.hyprland = {
         enable = true;
+        settings = {
+            exec-once = ''${startupScript}/bin/start'';
+        };
     };
 }
