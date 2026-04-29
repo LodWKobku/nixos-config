73161223d4aa80e134e298b1f6993bd455869518
LodWKobku <lodwkobku@gmail.com>
2026-04-29 17:52:10 +0200

    set up theme for rufi

--- components/home-manager/hyprland.nix
+++ components/home-manager/hyprland.nix
@@ -25,5 +25,11 @@
     # Launcher
     programs.rofi = {
         enable = true;
+        theme = "${pkgs.fetchFromGitHub {
+            owner = "adi1090x";
+            repo = "rofi";
+            rev = "master";
+            sha256 = "sha256-iUX0Quae06tGd7gDgXZo1B3KYgPHU+ADPBrowHlv02A=";
+        }}/files/launchers/type-3/style-1.rasi";
     };
 }
