c69a5fbe483d57bf425cee53f89e9c61d626977c
LodWKobku <lodwkobku@gmail.com>
2026-04-29 16:14:05 +0200

    installed fuzzel launcher

--- components/home-manager/hyprland.nix
+++ components/home-manager/hyprland.nix
@@ -6,12 +6,20 @@
         settings = {
             exec-once = [
                 "${pkgs.waybar}/bin/waybar"
+                "${pkgs.fuzzel}/bin/fuzzel"
+
             ];
         };
     };
     xdg.configFile."uwsm/env".source = "${config.home.sessionVariablesPackage}/etc/profile.d/hm-session-vars.sh"; 
 
-    programs.waybar = {
+    # Bar
+    programs.waybar = { 
+        enable = true;
+    };
+
+    # Launcher
+    programs.fuzzel = {
         enable = true;
     };
 }
