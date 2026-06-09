4ffc241a516319646aad981af98f9ca5d46597f4
LodWKobku <lodwkobku@gmail.com>
2026-06-09 09:54:37 +0200

    temp disable plasma-manager panels configuration

--- modules/features/system/plasma.nix
+++ modules/features/system/plasma.nix
@@ -55,22 +55,22 @@
                     }
                 ];
             };
-
-            panels = [ # Customize panel
-                {
-                    location = "bottom";
-                    lengthMode = "fill";
-                    widgets = [
-                        "org.kde.plasma.kickoff"
-                        "org.kde.plasma.pager"
-                        "org.kde.plasma.icontasks"
-                        "org.kde.plasma.marginsseparator"
-                        "org.kde.plasma.systemtray"
-                        "org.kde.plasma.digitalclock"
-                        "org.kde.plasma.showdesktop"
-                    ];
-                }
-            ];
+            
+            # panels = [ # Customize panel
+            #     {
+            #         location = "bottom";
+            #         lengthMode = "fill";
+            #         widgets = [
+            #             "org.kde.plasma.kickoff"
+            #             "org.kde.plasma.pager"
+            #             "org.kde.plasma.icontasks"
+            #             "org.kde.plasma.marginsseparator"
+            #             "org.kde.plasma.systemtray"
+            #             "org.kde.plasma.digitalclock"
+            #             "org.kde.plasma.showdesktop"
+            #         ];
+            #     }
+            # ];
         };
     };
 }
