d6b9ba52695f0ebf835f22e7f50570fb8c15751c
LodWKobku <lodwkobku@gmail.com>
2026-06-09 14:13:34 +0200

    uncommented panels in plasma-manager

--- modules/features/system/plasma.nix
+++ modules/features/system/plasma.nix
@@ -56,21 +56,21 @@
                 ];
             };
             
-            # panels = [ # Customize panel
-            #     {
-            #         location = "bottom";
-            #         lengthMode = "fill";
-            #         widgets = [
-            #             "org.kde.plasma.kickoff"
-            #             "org.kde.plasma.pager"
-            #             "org.kde.plasma.icontasks"
-            #             "org.kde.plasma.marginsseparator"
-            #             "org.kde.plasma.systemtray"
-            #             "org.kde.plasma.digitalclock"
-            #             "org.kde.plasma.showdesktop"
-            #         ];
-            #     }
-            # ];
+            panels = [ # Customize panel
+                {
+                    location = "bottom";
+                    lengthMode = "fill";
+                    widgets = [
+                        "org.kde.plasma.kickoff"
+                        "org.kde.plasma.pager"
+                        "org.kde.plasma.icontasks"
+                        "org.kde.plasma.marginsseparator"
+                        "org.kde.plasma.systemtray"
+                        "org.kde.plasma.digitalclock"
+                        "org.kde.plasma.showdesktop"
+                    ];
+                }
+            ];
         };
     };
 }
