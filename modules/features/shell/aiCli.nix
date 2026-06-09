909b88747b399ee26d16bba531d50190d84ef84d
LodWKobku <lodwkobku@gmail.com>
2026-06-09 18:10:10 +0200

    installed ai cli (huggingface, modelscope)

---
+++ modules/features/shell/aiCli.nix
@@ -0,0 +1,8 @@
+{ self, inputs, ... }: {
+    flake.nixosModules.aiCli = { pkgs, lib, config, ... }: {
+        environment.systemPackages = with pkgs; [
+            python313Packages.huggingface-hub
+            python313Packages.modelscope
+        ];
+    };
+}
--- modules/features/shell/shell.nix
+++ modules/features/shell/shell.nix
@@ -1,6 +1,9 @@
 { self, inputs, ... }: {
     flake.nixosModules.shell = { pkgs, lib, ... }: {
-        
+        imports = [
+            self.nixosModules.aiCli
+        ];
+
         home-manager.sharedModules = [
             self.homeModules.git
             self.homeModules.nix-init
