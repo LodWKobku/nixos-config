0032fd1157054c8df04a82da7ca7912ced21381d
LodWKobku <lodwkobku@gmail.com>
2026-06-09 16:57:23 +0200

    removed unnecessery default.nix file

--- modules/hosts/laptop/configuration.nix
+++ modules/hosts/laptop/configuration.nix
@@ -1,36 +1,39 @@
 { self, inputs, ... }: {
+  flake.nixosConfigurations.laptop = inputs.nixpkgs.lib.nixosSystem {
+    modules = [
+      {
+        imports = [
+          inputs.home-manager.nixosModules.home-manager
+          self.nixosModules.laptopHardware
+          self.nixosModules.basics
+          self.nixosModules.keyboard
+          
+          self.nixosModules.shell
+          self.nixosModules.plasma
+          self.nixosModules.stylix
+          self.nixosModules.browser
+          self.nixosModules.music
+        ];
 
-  flake.nixosModules.laptopConfig = { pkgs, lib, ... }: {
-    imports = [
-      inputs.home-manager.nixosModules.home-manager
-      self.nixosModules.laptopHardware
-      self.nixosModules.basics
-      self.nixosModules.keyboard
-      
-      self.nixosModules.shell
-      self.nixosModules.plasma
-      self.nixosModules.stylix
-      self.nixosModules.browser
-      self.nixosModules.music
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          backupFileExtension = "backup";
+          users.user = self.homeModules.user;
+        };
+
+        networking.hostName = "laptop";
+        
+        # Nh (nix helper)
+        programs.nh = {
+          enable = true;
+          clean.enable = true;
+          clean.extraArgs = "--keep-since 4d --keep 5";
+          flake = "/home/user/Documents/nixos-config#laptop";
+        };
+
+        system.stateVersion = "25.11";
+      }
     ];
-
-    home-manager = {
-      useGlobalPkgs = true;
-      useUserPackages = true;
-      backupFileExtension = "backup";
-      users.user = self.homeModules.user;
-    };
-
-    networking.hostName = "laptop";
-    
-    # Nh (nix helper)
-    programs.nh = {
-      enable = true;
-      clean.enable = true;
-      clean.extraArgs = "--keep-since 4d --keep 5";
-      flake = "/home/user/Documents/nixos-config#laptop";
-    };
-
-    system.stateVersion = "25.11";
   };
 }
