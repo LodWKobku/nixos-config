086d414f57c9dd1751244dfd1a20b407f161af8d
LodWKobku <lodwkobku@gmail.com>
2026-06-22 11:20:56 +0200

    uninstalled gemini-cli

--- modules/features/terminal/aiCli.nix
+++ modules/features/terminal/aiCli.nix
@@ -3,7 +3,6 @@
         environment.systemPackages = with pkgs; [
             python313Packages.huggingface-hub
             python313Packages.modelscope
-            gemini-cli
             antigravity-cli
         ];
     };
