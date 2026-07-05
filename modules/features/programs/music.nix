25613280c5a556f9ce7a9b3d13d24a1068dbd8df
LodWKobku <lodwkobku@gmail.com>
2026-07-05 13:58:14 +0200

    temp: installed libreoffice

--- modules/features/programs/music.nix
+++ modules/features/programs/music.nix
@@ -2,6 +2,7 @@
     flake.nixosModules.music = { pkgs, lib, config, ... }: {
         environment.systemPackages = with pkgs; [
             pear-desktop # Youtube music app
+            libreoffice
         ];
     };
 }
