60944992fa36511ab66ee824c58751e4a3aa117a
LodWKobku <lodwkobku@gmail.com>
2026-07-10 16:09:43 +0200

    installed steam

--- modules/features/programs/games.nix
+++ modules/features/programs/games.nix
@@ -1,9 +1,9 @@
 { self, inputs, ... }: {
     flake.nixosModules.games = { pkgs, lib, config, ... }: {
-#         programs.steam = {
-#             enable = true;
-#             remotePlay.openFirewall = true;
-#             dedicatedServer.openFirewall = true;
-#         };
+        programs.steam = {
+            enable = true;
+            remotePlay.openFirewall = true;
+            dedicatedServer.openFirewall = true;
+        };
     };
 }
