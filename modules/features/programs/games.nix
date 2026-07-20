df6817d94c5d41dcede16701133d1052e255511b
LodWKobku <lodwkobku@gmail.com>
2026-07-20 15:34:06 +0200

    installed gamemode and proton-ge for steam

--- modules/features/programs/games.nix
+++ modules/features/programs/games.nix
@@ -1,9 +1,14 @@
 { self, inputs, ... }: {
     flake.nixosModules.games = { pkgs, lib, config, ... }: {
+        # Steam
         programs.steam = {
             enable = true;
             remotePlay.openFirewall = true;
             dedicatedServer.openFirewall = true;
+            extraCompatPackages = [ pkgs.proton-ge-bin ];
         };
+        # Gamemode
+        programs.gamemode.enable = true;
+        users.users.user.extraGroups = [ "gamemode" ];
     };
 }
