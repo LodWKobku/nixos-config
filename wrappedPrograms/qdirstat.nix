2d712700e158f3076869eda215b08f8ffe8cbf03
LodWKobku <lodwkobku@gmail.com>
2026-08-10 14:43:26 +0200

    qdirstat: replaced lib.getExe with lib.getExe to aviod warning and removed unnecessery runtimeInputs

--- wrappedPrograms/qdirstat.nix
+++ wrappedPrograms/qdirstat.nix
@@ -3,11 +3,8 @@
         packages.qdirstat = inputs.wrappers.lib.wrapPackage {
             inherit pkgs;
             package = pkgs.qdirstat;
-            runtimeInputs = [
-                pkgs.lxqt.lxqt-sudo
-            ];
             # Run qdirstat as sudo by default
-            exePath = "${lib.getExe pkgs.lxqt.lxqt-sudo} -q ${lib.getExe pkgs.qdirstat}";
+            exePath = "${lib.getExe' pkgs.lxqt.lxqt-sudo "lxqt-sudo"} -q ${lib.getExe pkgs.qdirstat}";
         };
     };
 }
