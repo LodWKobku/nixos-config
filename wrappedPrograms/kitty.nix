45d436cd0d9d37512b646c0b6d730133192576aa
LodWKobku <lodwkobku@gmail.com>
2026-08-31 12:03:05 +0200

    kitty: bring back creating custom desktop file to better integrate with Plasma

--- wrappedPrograms/kitty.nix
+++ wrappedPrograms/kitty.nix
@@ -26,6 +26,22 @@
 
             # Adding alias "cmd" to desktop file for convinience
             aliases = [ "cmd" ];
+            constructFiles.kittyDesktop = {
+                content = (pkgs.makeDesktopItem {
+                        type = "Application";
+                        name = "kitty";
+                        desktopName = "kitty";
+                        genericName = "Terminal emulator";
+                        comment = "Fast, feature-rich, GPU based terminal";
+                        tryExec = "kitty";
+                        startupNotify = true;
+                        exec = "kitty";
+                        icon = "kitty";
+                        categories = [ "System" "TerminalEmulator" ];
+                        keywords = [ "cmd" ];
+                    }).text;
+                relPath = "share/applications/kitty.desktop";
+            };
         };
     };
 }
