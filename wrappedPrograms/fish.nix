92dad76aa1113c039583e55c316329492361acd0
LodWKobku <lodwkobku@gmail.com>
2026-08-31 13:00:54 +0200

    fish: remade disabling audocd to aviod pointless repeating for the orginal function

--- wrappedPrograms/fish.nix
+++ wrappedPrograms/fish.nix
@@ -11,61 +11,24 @@
             plugins = [
                 pkgs.fishPlugins.done
             ];
-            configFile.content = ''
+            configFile.content = 
+            let
+                cd-config = lib.splitString "\n" (builtins.readFile "${pkgs.fish.src}/share/functions/cd.fish");
+            in
+            ''
                 fish_config theme choose catppuccin-frappe
                 ${lib.getExe self'.packages.starship} init fish | source
                 ${lib.getExe pkgs.devenv} hook fish | source
 
-                # Rewrite cd command to disable autocd when command looks like directory
-                function cd --description 'Change directory'
-                    if test "$argv[1]" = '--'
-                        echo 'Autocd is disabled via config, use cd for changing directories' >&2
-                        return 1
-                    end
-                    set -l MAX_DIR_HIST 25
-
-                    # Skip history in subshells.
-                    if status is-command-substitution
-                        builtin cd $argv
-                        return $status
-                    end
-
-                    # Avoid set completions.
-                    set -l previous $PWD
-
-                    if test "$argv" = -
-                        if test "$__fish_cd_direction" = next
-                            nextd
-                        else
-                            prevd
+                # Disabling autocd in cd command
+                ${lib.concatLines ((lib.lists.take 5 cd-config) ++ [
+                    ''
+                        if test "$argv[1]" = '--'
+                            echo 'Autocd is disabled via config, use cd for changing directories' >&2
+                            return 1
                         end
-                        return $status
-                    end
-
-                    builtin cd $argv
-                    set -l cd_status $status
-
-                    if test $cd_status -eq 0 -a "$PWD" != "$previous"
-                        set -q dirprev
-                        or set -l dirprev
-                        set -q dirprev[$MAX_DIR_HIST]
-                        and set -e dirprev[1]
-
-                        set -U -q dirprev
-                        and set -U -a dirprev $previous
-                        or set -g -a dirprev $previous
-
-                        set -U -q dirnext
-                        and set -U -e dirnext
-                        or set -e dirnext
-
-                        set -U -q __fish_cd_direction
-                        and set -U __fish_cd_direction prev
-                        or set -g __fish_cd_direction prev
-                    end
-
-                    return $cd_status
-                end
+                    ''
+                ] ++ (lib.lists.drop 5 cd-config))}
             '';
         };
     };
