2bf5cd7cf1a0f44a80d0f764464b860dd8b925e8
LodWKobku <lodwkobku@gmail.com>
2026-09-02 20:11:34 +0200

    labwc: created placeholder files for labwc config

--- wrappedPrograms/labwc.nix
+++ wrappedPrograms/labwc.nix
@@ -3,6 +3,28 @@
         packages.labwc = inputs.wrappers.lib.wrapPackage {
             inherit pkgs;
             package = pkgs.labwc;
+            env = {
+                XDG_CONFIG_DIRS = pkgs.buildEnv {
+                    name = "labwc-config";
+                    extraPrefix = "/labwc";
+                    paths = 
+                        let
+                            writeFile = { name, text }: pkgs.writeTextFile {
+                                inherit name text;
+                                destination = "/${name}";
+                            };
+                        in [
+                            (writeFile {
+                                name = "file1";
+                                text = "placeholder";
+                            })
+                            (writeFile {
+                                name = "file2";
+                                text = "placeholder";
+                            })
+                        ];
+                };
+            };
         };
     };
 }
