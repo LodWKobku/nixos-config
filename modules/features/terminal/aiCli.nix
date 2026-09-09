9dc2b195a0d3e07a42ddc4f2ee6279b502e6b5a2
LodWKobku <lodwkobku@gmail.com>
2026-09-09 11:00:15 +0200

    updated system, temp disabled modelscope

--- modules/features/terminal/aiCli.nix
+++ modules/features/terminal/aiCli.nix
@@ -2,7 +2,7 @@
     flake.nixosModules.aiCli = { pkgs, lib, config, ... }: {
         environment.systemPackages = with pkgs; [
             python313Packages.huggingface-hub
-            python313Packages.modelscope
+            # python313Packages.modelscope # Temperary disabled becouse it is marked as unsafe https://tracker.security.nixos.org/issues/NIXPKGS-2026-2474
             antigravity-cli
             self.packages.${pkgs.stdenv.hostPlatform.system}.opencode
         ];
