223e7e2f5d39b231bc6b04437d68ca43511dce6d
LodWKobku <lodwkobku@gmail.com>
2026-09-10 10:51:54 +0200

    opencode: updated system and opencode package to use new tui-settings

--- wrappedPrograms/opencode.nix
+++ wrappedPrograms/opencode.nix
@@ -2,14 +2,10 @@
     perSystem = { pkgs, lib, self', ... }: {
         packages.opencode = inputs.wrapper-modules.wrappers.opencode.wrap {
             inherit pkgs;
-            # settings.tui = {
-            #     # "$schema"= "https://opencode.ai/tui.json";
-            #     "theme"= "tokyonight";
-            # };
-            env.OPENCODE_TUI_CONFIG = "${pkgs.writeText "tui.json" (builtins.toJSON {
+            tui-settings = {
                 "$schema"= "https://opencode.ai/tui.json";
                 "theme"= "catppuccin-frappe";
-            })}";
+            };
         };
     };
 }
