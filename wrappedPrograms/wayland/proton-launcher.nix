5b62917103ff3e22ab0f21bce6a5f18b7f162937
LodWKobku <lodwkobku@gmail.com>
2026-09-18 13:23:11 +0200

    proton-launcher: changed theme

--- wrappedPrograms/wayland/proton-launcher.nix
+++ wrappedPrograms/wayland/proton-launcher.nix
@@ -3,10 +3,18 @@
         packages.proton-launcher = pkgs.writeShellApplication {
             name = "proton-launcher";
             runtimeInputs = [ 
-                self'.packages.rofi
+                (inputs.wrapper-modules.wrappers.rofi.wrap {
+                     inherit pkgs;
+                    theme = "${pkgs.fetchFromGitHub {
+                        owner = "adi1090x";
+                        repo = "rofi";
+                        rev = "512a585fff6da5b2a90e5948059b062516ddb2e7";
+                        sha256 = "sha256-iUX0Quae06tGd7gDgXZo1B3KYgPHU+ADPBrowHlv02A=";
+                    }}/files/launchers/type-3/style-6.rasi";
+                })
             ];
             text = ''
-                choice=$(echo -e "proton-vpn\x00icon\x1fproton-vpn-logo\nproton-pass\x00icon\x1fproton-pass\nprotonmail-desktop\x00icon\x1fproton-mail" | rofi -show-icons -dmenu -p "Launch Proton App")
+                choice=$(echo -e "proton-vpn\x00icon\x1fproton-vpn-logo\nproton-pass\x00icon\x1fproton-pass\nprotonmail-desktop\x00icon\x1fproton-mail" | rofi -show-icons -theme-str "#listview { columns: 3; }" -dmenu -p "Launch Proton App")
 
                 case "$choice" in
                     proton-vpn) exec ${lib.getExe pkgs.proton-vpn} ;;
