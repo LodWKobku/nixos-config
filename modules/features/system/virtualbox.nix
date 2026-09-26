dac9c6e5a94e844fba7926f0a4d0bfe390a93bab
LodWKobku <lodwkobku@gmail.com>
2026-09-26 09:35:25 +0200

    installed and configured virt-manager

--- modules/features/system/virtualbox.nix
+++ modules/features/system/virtualbox.nix
@@ -4,6 +4,26 @@
             enable = true;
             enableExtensionPack = true;
         };
-        users.users.user.extraGroups = [ "user-with-access-to-virtualbox" ];
+        users.users.user.extraGroups = [ "user-with-access-to-virtualbox" "libvirtd" ];
+        
+        # Virt-manager
+        programs.dconf.enable = true;
+        virtualisation = {
+            spiceUSBRedirection.enable = true;
+            libvirtd = {
+                enable = true;
+                qemu.swtpm.enable = true;
+            };
+        };
+        services.spice-vdagentd.enable = true;
+        programs.virt-manager.enable = true;
+        environment.systemPackages = with pkgs; [
+            virt-viewer
+            spice spice-gtk
+            spice-protocol
+            virtio-win
+            win-spice
+            adwaita-icon-theme
+        ];
     };
 }
